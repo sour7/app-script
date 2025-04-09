2ee4034daa6c244817f846159820abada681740b
sour7 <sourabhdev273@gmail.com>
2025-04-09 19:49:31 +0530

    minor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { Inria_Sans } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 // import { Inter } from 'next/font/google'
  
